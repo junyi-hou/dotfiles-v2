72637b79a5dc9606e4c1334f7d3ad7bf35ea9459
junyi.hou <junyi.yi.hou@gmail.com>
2025-11-15 19:24:11 -0800

    add typst mode

--- modules/emacs.d/lisp/gatsby>text.el
+++ modules/emacs.d/lisp/gatsby>text.el
@@ -19,5 +19,19 @@
   
   )
 
+(use-package typst-ts-mode
+  :ensure (:host sourcehut :repo "meow_king/typst-ts-mode")
+  :custom-face
+  (typst-ts-markup-raw-blob-face ((t :inherit 'default)))
+  (typst-ts-markup-rawspan-lang-face ((t :inherit 'default :weight bold)))
+  :init
+  (gatsby>install-treesitter-grammar
+   'typst "https://github.com/uben0/tree-sitter-typst" "v0.11.0")
+  :hook (typst-ts-mode . display-line-numbers-mode)
+  :evil-bind
+  ((:maps typst-ts-mode-map :states insert)
+   ("SPC r r" . #'typst-ts-compile-and-preview)
+   ("SPC r o" . #'typst-ts-preview)))
+
 (provide 'gatsby>text)
 ;;; gatsby>text.el ends here
