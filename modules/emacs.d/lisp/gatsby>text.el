a1022474c1eb1d6fe1d695f48a37385ad55f1fb0
junyi.hou <junyi.yi.hou@gmail.com>
2025-11-16 10:11:51 -0800

    add tinymist for typst-lsp

--- modules/emacs.d/lisp/gatsby>text.el
+++ modules/emacs.d/lisp/gatsby>text.el
@@ -21,15 +21,55 @@
 
 (use-package typst-ts-mode
   :ensure (:host sourcehut :repo "meow_king/typst-ts-mode")
+  :defer t
   :custom-face
   (typst-ts-markup-raw-blob-face ((t :inherit 'default)))
   (typst-ts-markup-rawspan-lang-face ((t :inherit 'default :weight bold)))
+  :hook
+  (typst-ts-mode . eglot-ensure)
+  (typst-ts-mode . corfu-mode)
+  (typst-ts-mode . display-line-numbers-mode)
   :init
+  ;; tree-sitter
   (gatsby>install-treesitter-grammar
    'typst "https://github.com/uben0/tree-sitter-typst" "v0.11.0")
-  :hook (typst-ts-mode . display-line-numbers-mode)
+
+  ;; lsp
+  (add-to-list 'eglot-server-programs `(typst-ts-mode ,(expand-file-name ".tools/bin/tinymist" gatsby>dotfiles-repo-location)))
+
+  :config
+  ;; automatically watch file when running `typst-ts-compile-and-preview'
+  ;; let's watch only 1 file
+  (defvar gatsby>>typst-watching-file nil)
+
+  (defun gatsby>>typst-stop-watching (&rest _)
+    (when (and (file-equal-p current-file gatsby>>typst-watching-file)
+               (process-live-p (get-buffer-process typst-ts-watch-process-buffer-name)))
+      (typst-ts-watch-stop)
+      (setq gatsby>>typst-watching-file nil)))
+
+  (defun gatsby>>typst-start-watching (&rest _)
+    (cl-letf (((symbol-function #'message) #'ignore)
+              (current-file (buffer-file-name)))
+
+      ;; turn off watching if we are watching other file
+      (unless (and (file-equal-p current-file gatsby>>typst-watching-file)
+                   (y-or-n-p-insert-y (format "Watch is running on %s, kill it now? " gatsby>>typst-watching-file)))
+        (typst-ts-watch-stop)
+        (setq gatsby>>typst-watching-file nil))
+
+      ;; start watching if `gatsby>>typst-watching-file' is null
+      (unless gatsby>>typst-watching-file
+        (typst-ts-watch-start)
+        (setq gatsby>>typst-watching-file (buffer-file-name))
+
+        ;; setup stop watching hook
+        (add-hook 'kill-buffer-hook #'gatsby>>typst-stop-watching nil t))))
+
+  (advice-add #'typst-ts-compile-and-preview :after #'gatsby>>typst-start-watching)
+
   :evil-bind
-  ((:maps typst-ts-mode-map :states insert)
+  ((:maps typst-ts-mode-map :states normal)
    ("SPC r r" . #'typst-ts-compile-and-preview)
    ("SPC r o" . #'typst-ts-preview)))
 
