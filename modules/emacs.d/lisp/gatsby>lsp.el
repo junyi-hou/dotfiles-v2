ba927d79d5e2db9a9aa1cc7b2788fdb1883f06b6
junyi.hou <junyi.yi.hou@gmail.com>
2025-12-28 14:07:28 -0800

    replace eglot with lsp-mode

--- modules/emacs.d/lisp/gatsby>lsp.el
+++ modules/emacs.d/lisp/gatsby>lsp.el
@@ -17,7 +17,19 @@
   :hook
   (prog-mode . corfu-mode)
   (corfu-mode . corfu-popupinfo-mode)
-  (corfu-mode . gatsby>>set-capf-order)
+  :init
+  (defun gatsby>>insert-tab-if-no-compliaton-is-found ()
+    "Insert a tab when there's only blank char in front"
+    (when (save-excursion
+            (skip-chars-backward " \t")
+            (bolp))
+      (insert-tab)) nil)
+  (add-to-list 'completion-at-point-functions
+               #'gatsby>>insert-tab-if-no-compliaton-is-found)
+  (defun gatsby>add-to-capf (additional-capf-function)
+    "Make sure `gatsby>>insert-tab-if-no-compliaton-is-found' is always the first element in `capf'"
+    (let ((new-cdr (cons additional-capf-function (cdr completion-at-point-functions))))
+      (cons (car completion-at-point-functions) new-cdr)))
   :config
   (gatsby>defcommand gatsby>corfu-complete ()
     "Complete common parts of all the candidates, or insert the current selection.
@@ -32,19 +44,6 @@ Insert the current selection when
       (corfu-expand)))
   ;; for some reason I cannot bind <TAB> to any different variables, let's just override that function
   (advice-add #'corfu-complete :override #'gatsby>corfu-complete)
-  ;; If no completion is found, insert a tab:
-  (defun gatsby>>insert-tab-if-no-compliaton-is-found ()
-    (when (save-excursion
-            (skip-chars-backward " \t")
-            (bolp))
-      (insert-tab)) nil)
-  ;; make sure `gatsby>>insert-tab-if-no-compliaton-is-found' is always the first element in `capf'
-  (defun gatsby>>set-capf-order (&rest _)
-    (if-let ((tab (cl-find-if (lambda (it)
-                                (eq it #'gatsby>>insert-tab-if-no-compliaton-is-found))
-                              completion-at-point-functions)))
-        (setq-local completion-at-point-functions `(gatsby>>insert-tab-if-no-compliaton-is-found ,@(cl-remove #'gatsby>>insert-tab-if-no-compliaton-is-found completion-at-point-functions)))
-      (setq-local completion-at-point-functions `(gatsby>>insert-tab-if-no-compliaton-is-found ,@completion-at-point-functions))))
   :evil-bind
   ((:maps insert)
    ("<tab>" . #'completion-at-point)
@@ -67,140 +66,91 @@ Insert the current selection when
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles partial-completion)))))
 
-(gatsby>use-internal-pacakge eglot
+(use-package lsp-mode
+  :ensure (:host git
+           :repo "emacs-lsp/lsp-mode")
+  ;; :preface
+  ;; (setq lsp-use-plists t)
   :custom-face
-  (eglot-inlay-hint-face ((t (:height 1.0))))
-  (eglot-type-hint-face ((t (:height 1.0))))
-  (eglot-parameter-hint-face ((t (:height 1.0))))
+  (lsp-face-highlight-read ((t :inherit underline)))
+  (lsp-face-highlight-write ((t :inherit underline)))
   :custom
-  ;; (eglot-server-programs nil)
-  (eglot-extend-to-xref t)
-  :init
-  (setq eglot-server-programs nil)
-  (defun gatsby>>maybe-format-buffer ()
-    (add-hook 'before-save-hook
-              (defun gatsby>>maybe-format-before-save (&rest _)
-                (when (and (eglot-current-server)
-                           (eglot-server-capable
-                            :documentFormattingProvider))
-                  (eglot-format-buffer)))
-              nil
-              t))
-  :hook
-  (eglot-managed-mode . gatsby>>maybe-format-buffer)
-  (eglot-managed-mode . gatsby>>set-capf-order)
+  ;; increase data read from subrocess
+  (read-process-output-max (* 1024 1024))
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-signature-render-documentation t)
+  (lsp-signature-auto-activate nil)
+  (lsp-enable-file-watchers nil)
+  (lsp-enable-snippet nil)
   :evil-bind
-  ;; ((:maps (normal visual motion))
-  ;;  ("SPC" . nil)
-  ;;  ("SPC r" . nil)
-  ;;  ("SPC r a" . #'eglot-code-actions))
-  )
-
-(use-package consult-eglot
-  :ensure (:host github
-           :repo "mohkale/consult-eglot")
-  :config
-  (gatsby>defcommand gatsby>consult-symbols-or-line (prefix)
-    "Run `consult-lsp-file-symbols' if PREFIX, or `consult-line'."
-    (if prefix
-        (call-interactively #'consult-eglot-symbols)
-      (call-interactively #'consult-outline)))
-  :evil-bind
-  ((:maps (motion normal))
-   ([remap consult-outline] . #'gatsby>consult-symbols-or-line)))
+  ((:maps (normal visual))
+   ("SPC r a" . #'lsp-execute-code-action)
+   ("SPC r l" . #'xref-find-definitions)
+   ("SPC r L" . #'xref-find-references)
+   ("SPC r b" . #'xref-go-back)
+   ("SPC r f" . #'xref-go-forward)))
 
 ;; template system
-;; (use-package tempel
-;;   :ensure (:host github :repo "minad/tempel")
-;;   :custom (tempel-path (no-littering-expand-etc-file-name "tempel/*.eld"))
-;;   :evil-bind
-;;   ((:maps tempel-map)
-;;    ("C-g" . #'tempel-abort)
-;;    ("M-j" . #'tempel-next)
-;;    ("M-k" . #'tempel-prev))
-;;   :hook (corfu-mode . gatsby>>enable-tempel)
-;;   :commands (tempel--templates)
-;;   :init
-;;   ;; TODO: how to make sure that this is always the first in the `completion-at-point-functions' list?
-;;   (defun gatsby>>enable-tempel ()
-;;     (setq-local completion-at-point-functions `(gatsby>tempel-capf ,@completion-at-point-functions)))
-
-;;   (defun gatsby>tempel-capf ()
-;;     "CAPF that only complete the snippet names (and do not expand). If there's exact match, expand"
-;;     (when-let ((bounds (bounds-of-thing-at-point 'symbol)))
-;;       (let* ((name (buffer-substring-no-properties
-;;                     (car bounds) (cdr bounds)))
-;;              (sym (intern-soft name))
-;;              (templates (tempel--templates)))
-;;         (if-let ((template (assq sym templates)))
-;;             ;; exact match - expand
-;;             (list (car bounds) (cdr bounds) (list template)
-;;                   :category 'tempel
-;;                   :exclusive 'no
-;;                   :exit-function (apply-partially #'tempel--exit templates nil))
-;;           (let ((template-names (mapcar #'car templates)))
-;;             (list (car bounds) (cdr bounds) template-names
-;;                   :category 'templ
-;;                   :company-kind (lambda (_) 'snippet)
-;;                   :exclusive 'no
-;;                   :company-doc-buffer
-;;                   (apply-partially #'tempel--info-buffer templates
-;;                                    (lambda (elts)
-;;                                      (insert (tempel--print-template elts))
-;;                                      (tempel--insert-doc elts)
-;;                                      (current-buffer)))
-;;                   :company-location
-;;                   (apply-partially #'tempel--info-buffer templates
-;;                                    (lambda (elts)
-;;                                      (pp (cl-loop for x in elts
-;;                                                   until (keywordp x) collect x)
-;;                                          (current-buffer))
-;;                                      (tempel--insert-doc elts)
-;;                                      (list (current-buffer))))
-;;                   :annotation-function
-;;                   (and tempel-complete-annotation
-;;                        (apply-partially #'tempel--annotate
-;;                                         templates tempel-complete-annotation " ")))))))))
-
-;; (use-package eglot-tempel
-;;   :ensure (:host github :repo "fejfighter/eglot-tempel")
-;;   :hook (elpaca-after-init . eglot-tempel-mode))
-
-(gatsby>use-internal-pacakge xref
-  :demand t
-  :custom
-  (xref-prompt-for-identifier nil)
-  (xref-show-definitions-function #'consult-xref)
-  (xref-show-xrefs-function #'consult-xref)
-  ;; :evil-bind
-  ;; ((:maps (normal visual motion))
-  ;;  ("SPC" . nil)
-  ;;  ("SPC r" . nil)
-  ;;  ("SPC r l" . #'xref-find-definitions)
-  ;;  ("SPC r L" . #'xref-find-references)
-  ;;  ("SPC r b" . #'xref-go-back)
-  ;;  ("SPC r f" . #'xref-go-forward))
-  )
-
-;; ;; TODO: until I figure out a way to install emacs-lsp-booster reliably
-;; (use-package eglot-booster
-;;  :ensure (:host github :repo "jdtsmith/eglot-booster")
-;;  :custom (eglot-booster-io-only (>= emacs-major-version 30))
-;;   :hook (elpaca-after-init . eglot-booster-mode))
-
-;; (add-to-list 'eglot-server-programs
-;;              `(python-ts-mode . ,(eglot-alternatives
-;;                 '("pylsp" "pyls" ("basedpyright-langserver" "--stdio")
-;;                   ("pyright-langserver" "--stdio")
-;;                   ("pyrefly" "lsp")
-;;                   "jedi-language-server" ("ruff" "server") "ruff-lsp"))
-;;              ))
+(use-package tempel
+  :ensure (:host github
+           :repo "minad/tempel")
+  :custom (tempel-path (no-littering-expand-etc-file-name "tempel/*.eld"))
+  :evil-bind
+  ((:maps tempel-map)
+   ("C-g" . #'tempel-abort)
+   ("M-j" . #'tempel-next)
+   ("M-k" . #'tempel-prev))
+  :hook (corfu-mode . gatsby>>enable-tempel)
+  :commands (tempel--templates)
+  :init
+  (defun gatsby>>enable-tempel ()
+    (gatsby>add-to-capf #'gatsby>tempel-capf))
+  (defun gatsby>tempel-capf ()
+    "CAPF that only complete the snippet names (and do not expand). If there's exact match, expand"
+    (when-let ((bounds (bounds-of-thing-at-point 'symbol)))
+      (let* ((name (buffer-substring-no-properties
+                    (car bounds)
+                    (cdr bounds)))
+             (sym (intern-soft name))
+             (templates (tempel--templates)))
+        (if-let ((template (assq sym templates)))
+            ;; exact match - expand
+            (list (car bounds)
+                  (cdr bounds)
+                  (list template)
+                  :category 'tempel
+                  :exclusive 'no
+                  :exit-function (apply-partially #'tempel--exit templates nil))
+          (let ((template-names (mapcar #'car templates)))
+            (list (car bounds)
+                  (cdr bounds) template-names
+                  :category 'templ
+                  :company-kind (lambda (_) 'snippet)
+                  :exclusive 'no
+                  :company-doc-buffer
+                  (apply-partially #'tempel--info-buffer templates
+                                   (lambda (elts)
+                                     (insert (tempel--print-template elts))
+                                     (tempel--insert-doc elts)
+                                     (current-buffer)))
+                  :company-location
+                  (apply-partially #'tempel--info-buffer templates
+                                   (lambda (elts)
+                                     (pp (cl-loop for x in elts
+                                                  until (keywordp x) collect x)
+                                         (current-buffer))
+                                     (tempel--insert-doc elts)
+                                     (list (current-buffer))))
+                  :annotation-function
+                  (and tempel-complete-annotation
+                       (apply-partially #'tempel--annotate
+                                        templates tempel-complete-annotation " ")))))))))
 
 ;; display flymake information in a childframe
 (use-package flymake-childframe
   :ensure (:host github
            :repo "junyi-hou/flymake-childframe")
-  :hook ((flymake eglot-managed-mode) . flymake-childframe-mode))
+  :hook ((flymake lsp-managed-mode) . flymake-childframe-mode))
 
 (use-package eldoc-mouse
   :ensure (:host github
