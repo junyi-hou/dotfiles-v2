1801a21be62415d1a35b9b795993ae5442029bec
junyi.hou <junyi.yi.hou@gmail.com>
2026-04-04 16:03:22 -0700

    make profile tramp-compatible in remote

--- modules/emacs.d/lisp/gatsby>remote.el
+++ modules/emacs.d/lisp/gatsby>remote.el
@@ -22,34 +22,37 @@
   (envrc-remote t)
 
   :config
+
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
   (with-eval-after-load 'compile
     (remove-hook
      'compilation-mode-hook #'tramp-compile-disable-ssh-controlmaster-options)))
 
-(use-package tramp-rpc
-  :preface
-  ;; TODO - remove manual dependency installation when upstream supports it
-  (use-package msgpack
-    :ensure (:host github :repo "xuchunyang/msgpack.el"))
-  :ensure (:host github :repo "ArthurHeymans/emacs-tramp-rpc")
-  :config
+;; (use-package tramp-rpc
+;;   :preface
+;;   ;; TODO - remove manual dependency installation when upstream supports it
+;;   (use-package msgpack
+;;     :ensure (:host github :repo "xuchunyang/msgpack.el"))
+;;   :ensure (:host github :repo "ArthurHeymans/emacs-tramp-rpc")
+;;   :config
 
-  (defun gatsby>>rpc-eshell/ssh (&rest args)
-    "Use tramp-rpc for ssh."
-    (let* ((_args
-            (thread-last
-             args
-             (mapcar (lambda (i) (format "%s" i)))
-             (cl-remove-if (lambda (s) (string-prefix-p "-" s)))))
-           (host (format "/rpc:%s:~" (car _args))))
-      (if (> (length _args) 1)
-          (user-error
-           (format
-            "eshell/ssh accepts only one argument in the form of user@hostname, got %s instead"
-            _args))
-        (apply #'eshell/cd `(,host)))))
+;;   (defun gatsby>>rpc-eshell/ssh (&rest args)
+;;     "Use tramp-rpc for ssh."
+;;     (let* ((_args
+;;             (thread-last
+;;              args
+;;              (mapcar (lambda (i) (format "%s" i)))
+;;              (cl-remove-if (lambda (s) (string-prefix-p "-" s)))))
+;;            (host (format "/rpc:%s:~" (car _args))))
+;;       (if (> (length _args) 1)
+;;           (user-error
+;;            (format
+;;             "eshell/ssh accepts only one argument in the form of user@hostname, got %s instead"
+;;             _args))
+;;         (apply #'eshell/cd `(,host)))))
 
-  (advice-add #'eshell/ssh :override #'gatsby>>rpc-eshell/ssh))
+;;   (advice-add #'eshell/ssh :override #'gatsby>>rpc-eshell/ssh))
 
 (provide 'gatsby>remote)
 ;;; gatsby>remote.el ends here
