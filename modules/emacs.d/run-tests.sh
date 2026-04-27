e970ea7d07c8887f65443d40bd4e07db59549935
junyi.hou <junyi.yi.hou@gmail.com>
2026-04-26 22:20:51 -0700

    test(emacs.d): remove error output filter from test runner

--- modules/emacs.d/run-tests.sh
+++ modules/emacs.d/run-tests.sh
@@ -10,4 +10,4 @@ emacs --batch \
   --eval '(advice-add #'"'"'gatsby>>update-elpaca-lock-file :override #'"'"'ignore)' \
   --eval '(add-to-list (quote load-path) (expand-file-name "tests"))' \
   $(for f in tests/*.el; do echo "-l $f"; done) \
-  -f ert-run-tests-batch-and-exit 2>&1 | grep -v "^Error in kill-emacs-hook:"
+  -f ert-run-tests-batch-and-exit
