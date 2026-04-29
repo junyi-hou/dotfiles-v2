641f13d6662eb99be1142bade46bc2e8cf41bde9
junyi.hou <junyi.yi.hou@gmail.com>
2026-04-29 00:09:30 -0700

    config(dir-locals): different default tests in/out emacs.d

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,9 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((gatsby>eglot-auto-format-before-save . t)
-         (gatsby>project-tests-command . ("make" "test-emacs"))
-         (gatsby>get-individual-test-function . (lambda ()
-           (let ((choice (completing-read "Run tests: " '("emacs" "scripts") nil t)))
-             (if (equal choice "emacs")
-                 '("make" "test-emacs")
-               '("make" "test-scripts"))))))))
+         (gatsby>project-tests-command . ("make" "test-scripts")))))
