592bbb05c158efe6e84916decd77fc80a7d2e800
junyi.hou <junyi.yi.hou@gmail.com>
2026-04-29 00:09:30 -0700

    config(dir-locals): different default tests in/out emacs.d

--- modules/emacs.d/.dir-locals.el
+++ modules/emacs.d/.dir-locals.el
@@ -2,4 +2,5 @@
 
 ((emacs-lisp-mode
   (elisp-flymake-byte-compile-load-path . ("." "./lisp" "../lisp"))
-  (elisp-autofmt-load-packages-local . ("use-package" "use-package-core" "gatsby>>utility" "evil-macros" "evil-common" "cl-lib" "cl-seq" "cl-macs"))))
+  (elisp-autofmt-load-packages-local . ("use-package" "use-package-core" "gatsby>>utility" "evil-macros" "evil-common" "cl-lib" "cl-seq" "cl-macs")))
+ (nil . ((gatsby>project-tests-command . ("make" "test-emacs")))))
