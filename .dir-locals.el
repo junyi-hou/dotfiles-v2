88df6977eb1b2d55f7587e996e4eceb6ea6fcc22
junyi.hou <junyi.yi.hou@gmail.com>
2026-04-28 19:24:09 -0700

    config(dir-locals): add test command and test selection configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,10 @@
 ;;; Directory Local Variables -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((gatsby>eglot-auto-format-before-save . t))))
+((nil . ((gatsby>eglot-auto-format-before-save . t)
+         (gatsby>project-tests-command . ("make" "test-emacs"))
+         (gatsby>get-individual-test-function . (lambda ()
+           (let ((choice (completing-read "Run tests: " '("emacs" "scripts") nil t)))
+             (if (equal choice "emacs")
+                 '("make" "test-emacs")
+               '("make" "test-scripts"))))))))
