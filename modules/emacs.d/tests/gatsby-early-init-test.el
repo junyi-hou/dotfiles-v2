62fc0b2bbae454f99c8934bf5487235e1dcabd0f
junyi.hou <junyi.yi.hou@gmail.com>
2026-05-01 18:39:36 -0700

    test(emacs): add test for lock file ref validation

--- modules/emacs.d/tests/gatsby-early-init-test.el
+++ modules/emacs.d/tests/gatsby-early-init-test.el
@@ -30,5 +30,25 @@
     (should (equal (map-elt result 'my-pkg)
                    `(:source "elpaca-menu-lock-file" :recipe ,new-recipe)))))
 
+(ert-deftest gatsby>>elpaca-lock-file--checked-out-refs-match ()
+  "Each package in the lock file has the same HEAD as its :ref in the recipe."
+  (let* ((lock-contents
+          (with-temp-buffer
+            (insert-file-contents elpaca-lock-file)
+            (car (read-from-string (buffer-string)))))
+         (mismatches nil))
+    (dolist (entry lock-contents)
+      (let* ((id (car entry))
+             (recipe (plist-get (cdr entry) :recipe))
+             (expected-ref (plist-get recipe :ref))
+             (source-dir (expand-file-name (symbol-name id) elpaca-sources-directory)))
+        (when (and expected-ref (file-directory-p source-dir))
+          (let* ((default-directory source-dir)
+                 (actual-ref (string-trim
+                              (shell-command-to-string "git rev-parse HEAD"))))
+            (unless (string= expected-ref actual-ref)
+              (push (list id expected-ref actual-ref) mismatches))))))
+    (should-not mismatches)))
+
 (provide 'gatsby-early-init-test)
 ;;; gatsby-early-init-test.el ends here
