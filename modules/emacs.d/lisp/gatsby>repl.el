23173a57c51637352d7f9b561c83308c04f9aa84
junyi.hou <junyi.yi.hou@gmail.com>
2026-05-29 17:30:51 -0700

    feat(repl): add supports of starting repl on remote files

--- modules/emacs.d/lisp/gatsby>repl.el
+++ modules/emacs.d/lisp/gatsby>repl.el
@@ -24,6 +24,10 @@
 
   (defvar gatsby>jupyter-managed-mode-map (make-sparse-keymap))
 
+  (defvar gatsby>>jupyter-sessions nil
+    "List of Jupyter sessions started by gatsby commands.
+Each entry is a plist with :label (string) and :procs (list of processes).")
+
   (define-minor-mode gatsby>jupyter-managed-mode
     "Minor mode that provides keybinds and commands to major-modes that support jupyter repl."
     :lighter nil
@@ -107,29 +111,131 @@ normal state.  Otherwise evaluate from the previous cell separator (or
             default-directory)))
        (jupyter-servers))))
 
+  (defun gatsby>>find-free-port ()
+    "Return a free local TCP port number."
+    (let* ((proc
+            (make-network-process
+             :name "gatsby-port-finder"
+             :server t
+             :host "127.0.0.1"
+             :service t
+             :family 'ipv4
+             :noquery t))
+           (port (process-contact proc :service)))
+      (delete-process proc)
+      port))
+
+  (defun gatsby>>jupyter-start-remote-repl (code-buffer)
+    "Launch a Jupyter server on the TRAMP remote host of CODE-BUFFER.
+Forwards the server port over SSH and connects a REPL to CODE-BUFFER."
+    (let* ((dir
+            (with-current-buffer code-buffer
+              default-directory))
+           (vec (tramp-dissect-file-name dir))
+           (host (tramp-file-name-host vec))
+           (remote-dir (tramp-file-name-localname vec))
+           (local-port (gatsby>>find-free-port))
+           (remote-port (+ 49152 (random 16383)))
+           (log-buf (get-buffer-create (format " *jupyter-remote-%s*" host)))
+           connected)
+      (message "Starting Jupyter on %s (local port %d -> remote port %d)..."
+               host
+               local-port
+               remote-port)
+      (let
+          ((proc
+            (start-process
+             (format "jupyter-remote-%s" host) log-buf "ssh"
+             "-L"
+             (format "127.0.0.1:%d:127.0.0.1:%d" local-port remote-port)
+             host
+             (format
+              "cd %s && direnv exec . jupyter server --no-browser --port %d --IdentityProvider.token=''"
+              (shell-quote-argument remote-dir) remote-port))))
+        (set-process-filter
+         proc
+         (lambda (_proc output)
+           (with-current-buffer log-buf
+             (goto-char (point-max))
+             (insert output))
+           (when (and (not connected) (string-match "is running" output))
+             (setq connected t)
+             (message "Jupyter ready on local port %d; connecting..." local-port)
+             (run-with-timer
+              0.5 nil
+              (lambda ()
+                (condition-case err
+                    (let* ((server
+                            (jupyter-server
+                             :url (format "http://127.0.0.1:%d" local-port)))
+                           (kernel
+                            (jupyter-completing-read-kernelspec
+                             (jupyter-kernelspecs server))))
+                      (jupyter-run-server-repl server kernel nil code-buffer nil t)
+                      (push (list
+                             :label (format "remote: %s:%s" host remote-dir)
+                             :procs (list proc))
+                            gatsby>>jupyter-sessions)
+                      (jupyter-repl-pop-to-buffer))
+                  (error
+                   (message "Failed to connect to remote Jupyter: %s" err)
+                   (kill-process proc)))))))))))
+
   (gatsby>defcommand gatsby>jupyter-start-or-switch-to-repl (connect)
     "Switch to REPL associated the current buffer."
     (if (gatsby>>has-jupyter-kernel)
         (jupyter-repl-pop-to-buffer)
       (let ((code-buffer (current-buffer)))
-        (if connect
-            ;; if prefix arg is given, connect to a knowen server
-            (let ((current-prefix-arg t))
-              (call-interactively #'jupyter-connect-server-repl))
-          ;; otherwise check whether there's a server running in `cwd'
+        (cond
+         (connect
+          (let ((current-prefix-arg t))
+            (call-interactively #'jupyter-connect-server-repl))
+          (jupyter-repl-pop-to-buffer))
+         ((file-remote-p
+           (with-current-buffer code-buffer
+             default-directory))
+          (gatsby>>jupyter-start-remote-repl code-buffer))
+         (t
           (let* ((cwd
                   (with-current-buffer code-buffer
                     default-directory))
                  (server (gatsby>>get-server-at cwd)))
-            ;; if not, start a new server
             (unless server
               (let ((port (jupyter-launch-notebook)))
-                (setq server
-                      (jupyter-server :url (format "http://localhost:%s" port)))))
+                (setq server (jupyter-server :url (format "http://localhost:%s" port))))
+              (push (list
+                     :label (format "local: %s" cwd)
+                     :procs (list (jupyter-notebook-process server)))
+                    gatsby>>jupyter-sessions))
             (let ((kernel
                    (jupyter-completing-read-kernelspec (jupyter-kernelspecs server))))
-              (jupyter-run-server-repl server kernel nil code-buffer nil t))))
-        (jupyter-repl-pop-to-buffer))))
+              (jupyter-run-server-repl server kernel nil code-buffer nil t)))
+          (jupyter-repl-pop-to-buffer))))))
+
+  (gatsby>defcommand gatsby>jupyter-kill-notebook ()
+    "Select and terminate a tracked Jupyter notebook server."
+    (setq gatsby>>jupyter-sessions
+          (seq-filter
+           (lambda (s) (seq-some #'process-live-p (plist-get s :procs)))
+           gatsby>>jupyter-sessions))
+    (if (null gatsby>>jupyter-sessions)
+        (message "No active Jupyter sessions.")
+      (let* ((choice
+              (completing-read "Kill session: "
+                               (mapcar
+                                (lambda (s) (plist-get s :label))
+                                gatsby>>jupyter-sessions)
+                               nil t))
+             (session
+              (seq-find
+               (lambda (s) (equal (plist-get s :label) choice))
+               gatsby>>jupyter-sessions)))
+        (dolist (p (plist-get session :procs))
+          (when (process-live-p p)
+            (kill-process p)))
+        (setq gatsby>>jupyter-sessions
+              (seq-remove (lambda (s) (eq s session)) gatsby>>jupyter-sessions))
+        (message "Terminated: %s" choice))))
 
   (gatsby>defcommand gatsby>jupyter-goto-last-prompt ()
     (goto-char (point-max))
@@ -162,6 +268,7 @@ normal state.  Otherwise evaluate from the previous cell separator (or
    (">" . #'gatsby>jupyter-next-cell)
    ("<" . #'gatsby>jupyter-prev-cell)
    ("SPC r o" . #'gatsby>jupyter-start-or-switch-to-repl)
+   ("SPC r k" . #'gatsby>jupyter-kill-notebook)
    ("SPC r z" . #'jupyter-repl-associate-buffer)
 
    (:maps gatsby>jupyter-managed-mode-map :states (normal visual))
