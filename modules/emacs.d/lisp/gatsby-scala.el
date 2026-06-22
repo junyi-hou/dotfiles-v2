d7ca69ba106b3ed8192bcdf3eb848f65b0d063a4
junyi.hou <junyi.yi.hou@gmail.com>
2026-06-22 08:14:17 -0700

    fix(scala): pin grammar to ABI-compatible version

--- modules/emacs.d/lisp/gatsby-scala.el
+++ modules/emacs.d/lisp/gatsby-scala.el
@@ -9,7 +9,9 @@
   :init
   (gatsby>install-treesitter-grammar
    'scala "https://github.com/tree-sitter/tree-sitter-scala"
-   "v0.25.0"))
+   ;; v0.25.0 regenerates the parser with tree-sitter ABI 15, which is not
+   ;; accepted by Ubuntu's older packaged tree-sitter/Emacs builds.
+   "v0.24.1"))
 
 (provide 'gatsby-scala)
 ;;; gatsby-scala.el ends here
